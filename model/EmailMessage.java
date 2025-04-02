3bebf947c3907499d595e15092ff1ec92fe1a158
UnknOwnkisuke <phenix_jop@live.com>
2025-04-02 08:52:23 -0500

    1% completed

---
+++ model/EmailMessage.java
@@ -0,0 +1,66 @@
+package model;
+
+
+import java.security.Timestamp;
+
+public class EmailMessage {
+
+    private int id;
+    private String phoneNumber;
+    private String message;
+    private String url;
+    private String email;
+    private Timestamp dateAdded;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Timestamp getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(Timestamp dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+
+}
--- model/User.java
+++ model/User.java
@@ -2,9 +2,18 @@ package model;
 
 public class User {
 
+    private int id;
     private String username;
     private String password;
     private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getUsername() {
         return username;
